db6438ac93e012c42826a7b5baf09dcd019ac3f5
VinhChu <vinhchu.cva@gmail.com>
2020-04-27 17:38:14 +0700

    update 27042020

---
+++ assets/js/dpotferstudio/dpotferstudio.js
@@ -0,0 +1,9 @@
+(function ($) {
+    $("#fullpage").fullpage({
+        verticalCentered: false,
+        v2compatible: true,
+        css3: true,
+        recordHistory: true,
+        scrollingSpeed: 1600,
+    });
+})(jQuery);
