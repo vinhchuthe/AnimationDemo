66507b811adcf75bc3e06230cfbd14a56dba09bf
VinhChu <vinhchu.cva@gmail.com>
2020-07-04 09:25:49 +0700

    update vietjet

--- assets/js/vietjet/vietjet.js
+++ assets/js/vietjet/vietjet.js
@@ -6,3 +6,8 @@ var swiper = new Swiper('.swiper-container', {
     mousewheel: true,
 });
 
+$("#slide27 .back-to-top").on("click", function () {
+    console.log("back to top");
+    swiper.slideTo(0, 1500, false);
+});
+
